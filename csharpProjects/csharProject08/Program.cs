7dc284c32c68a914f53f3fd7ac8a2f1f3a8d8cb9
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 16:14:17 -0300

    3 dados

--- csharpProjects/csharProject08/Program.cs
+++ csharpProjects/csharProject08/Program.cs
@@ -1,1 +1,13 @@
 ﻿// game do Dado
+Random dice = new();
+
+int dado1 = dice.Next(1, 7);
+int dado2 = dice.Next(1, 7);
+int dado3 = dice.Next(1 ,7);
+
+int total = dado1 + dado2 + dado3;
+
+Console.WriteLine($"O valor 1: {dado1}");
+Console.WriteLine($"O valor 2: {dado2}");
+Console.WriteLine($"O valor 3: {dado3}");
+Console.WriteLine($"O valor total: {total}");
