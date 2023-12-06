26213106f41806439774313fa4aee7cb14bdbe57
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 11:48:53 -0300

    nameage

--- csharpProjects/csharProject02/Program.cs
+++ csharpProjects/csharProject02/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string nome = "Carlos";
+int idade = 21;
+decimal altura = 1.95m;
+
+Console.Write($"Ola, {nome}!");
+Console.Write($"{nome} tem {idade} anos de idade.");
+Console.WriteLine($"Sua altura e de {altura}.");
