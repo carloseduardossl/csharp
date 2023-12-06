a1d44d156d6119a8e3d1c83729bf0e50f6b1f454
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 11:03:33 -0300

    bro

--- csharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ csharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a5ce8e4caba96412b771041a91bdcbaab958c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e6813cd29258b145e5283ab3c075abde6d1d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
