9077310eaf8781c79f9f3abaadf54a9d55f2d017
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 11:55:02 -0300

    .

---
+++ csharpProjects/csharProject03/Program.cs
@@ -0,0 +1,8 @@
+﻿decimal n1 = 5.6m;
+decimal n2 = 7.6m;
+decimal n3 = 8.2m;
+
+decimal soma = n1 + n2 + n3;
+decimal media = soma / 3;
+media = Math.Round(media, 1);
+Console.WriteLine($"A media do alundo e {media}");
