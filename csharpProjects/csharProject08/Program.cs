2d38e5b1975a6c6a84cb78d86e61eae580a533e0
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 16:37:40 -0300

    666

--- csharpProjects/csharProject08/Program.cs
+++ csharpProjects/csharProject08/Program.cs
@@ -7,11 +7,24 @@ int dado3 = dice.Next(1 ,7);
 
 int total = dado1 + dado2 + dado3;
 
+if (dado2 == dado3 )
+{
+    total++;
+
+    Console.WriteLine("Voce ganhou +1 ponto");
+}
+
+if (dado1 == dado2)
+{
+    total++;
+    total++;
+    Console.WriteLine("Voce ganhou +2 pontos");
+}
+
 Console.WriteLine($"O valor 1: {dado1}");
 Console.WriteLine($"O valor 2: {dado2}");
 Console.WriteLine($"O valor 3: {dado3}");
 Console.WriteLine($"O valor total: {total}");
-
 
 if (total > 14)
 {
