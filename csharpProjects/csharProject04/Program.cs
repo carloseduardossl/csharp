077a8b7a08ceed1d9f5588ac3ca74034453b36a3
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 12:25:26 -0300

    finaly

--- csharpProjects/csharProject04/Program.cs
+++ csharpProjects/csharProject04/Program.cs
@@ -32,17 +32,24 @@ decimal materia2soma = materia2nota1 + materia2nota2 + materia2nota3;
 decimal materia3soma = materia3nota1 + materia3nota2 + materia3nota3;
 decimal materia4soma = materia4nota1 + materia4nota2 + materia4nota3;
 
-decimal materia1media = materia1soma \ 3;
-decimal materia2media = materia2soma \ 3;
-decimal materia3media = materia3soma \ 3;
-decimal materia4media = materia4soma \ 3;
+decimal materia1media = materia1soma / 3;
+decimal materia2media = materia2soma / 3;
+decimal materia3media = materia3soma / 3;
+decimal materia4media = materia4soma / 3;
 
-decimal somageral = materia1soma + materia2soma + materia3soma + materia4soma;
-decimal mediageral = somageral \ 4;
+materia1media = Math.Round(materia1media, 1);
+materia2media = Math.Round(materia2media, 1);
+materia3media = Math.Round(materia3media, 1);
+materia4media = Math.Round(materia4media, 1);
 
-Console.WriteLine($"Nome do estudante:\t{nomeEstudante}");
-Console.WriteLine($"Materias:\t\t\t AV1:\t\tAV2:\t\tAV3:\t\t Media Final:")
-Console.WriteLine($"{materia1}\t\t\t{materia1nota1}\t\t{materia1nota2}\t\t{materia1nota3}\t\t{materia1media}");
+decimal somageral = materia1media + materia2media + materia3media + materia4media;
+decimal mediageral = (somageral / 4);
+mediageral = Math.Round(mediageral, 1);
+
+Console.WriteLine($"Nome do estudante:\t{nomeEstudante}\n");
+Console.WriteLine($"Materias:\t\t\t AV1:\t\tAV2:\t\tAV3:\t Media Final:");
+Console.WriteLine($"{materia1}\t\t\t\t{materia1nota1}\t\t{materia1nota2}\t\t{materia1nota3}\t\t{materia1media}");
 Console.WriteLine($"{materia2}\t\t\t{materia2nota1}\t\t{materia2nota2}\t\t{materia2nota3}\t\t{materia2media}");
-Console.WriteLine($"{materia3}\t\t\t{materia3nota1}\t\t{materia3nota2}\t\t{materia3nota3}\t\t{materia3media}");
-Console.WriteLine($"");
+Console.WriteLine($"{materia3}\t\t{materia3nota1}\t\t{materia3nota2}\t\t{materia3nota3}\t\t{materia3media}");
+Console.WriteLine($"{materia4}\t\t\t{materia4nota1}\t\t{materia4nota2}\t\t{materia4nota3}\t\t{materia4media}");
+Console.WriteLine($"Media geral:\t\t{mediageral}");
