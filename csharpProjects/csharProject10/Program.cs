2723d47d239a8051d82b87c6b13e272395135c56
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-06 21:26:12 -0300

    withB

---
+++ csharpProjects/csharProject10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Desafio do if, else, elseif, foreach e array
+string[] elementos = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+int withb = 0;
+foreach (string code in elementos)
+{
+    withb++;
+    if(code.StartsWith("B")){
+        Console.WriteLine($"o indice {withb} comeca com B: {code}");
+    }
+}
