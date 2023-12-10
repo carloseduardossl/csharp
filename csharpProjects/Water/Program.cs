a3a453926eccda5cfbb8668e5e87f63b5fcb3aae
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-10 14:41:24 -0300

    WATER

---
+++ csharpProjects/Water/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ola! Qual o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual seu peso?");
+decimal peso1 = Convert.ToDecimal(Console.ReadLine());
+decimal peso2 = (decimal)peso1 / 10; 
+
+static void CalculoDaAgua (decimal peso2)
+{
+    decimal quantidadeAgua = (35 * peso2) / 1000;
+    quantidadeAgua = Math.Round(quantidadeAgua, 1);
+    Console.WriteLine($"Diariamente, voce precisa beber {quantidadeAgua}L litros de agua");
+
+}
+CalculoDaAgua(peso2);
