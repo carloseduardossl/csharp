0d80fba98b0dc90356041decdb63c478775d28c4
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-11 16:53:07 -0300

    operadores ligicos com numeros e validação de e-mail simples

---
+++ csharpProjects/csharProject18/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine( 2 > 1 );
+Console.WriteLine( 3 < 2 );
+Console.WriteLine( 2 >= 2);
+Console.WriteLine( 1 <= 0);
+
+//---------------------------------------------------------------------------------
+
+string email = Console.ReadLine();
+if (email.Contains("@gmail.com") || email.Contains("@hotmail.com"))
+    Console.WriteLine("esse email e valido");
+else 
+    Console.WriteLine("esse email e invalido");
