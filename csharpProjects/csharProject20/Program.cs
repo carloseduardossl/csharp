ba6121c7b0d581b56d8d947b9181f77d6c96a2b3
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-11 19:25:32 -0300

    acesso de rede por privilegio e permissão

---
+++ csharpProjects/csharProject20/Program.cs
@@ -0,0 +1,17 @@
+﻿string permission = Console.ReadLine();
+int level = Convert.ToInt32(Console.ReadLine());
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+        Console.WriteLine("Bem vindo, Super Admin!");
+    else
+        Console.WriteLine("Bem vindo Admin");
+}
+else if (permission.Contains("Gerente"))
+{
+    if (level > 20)
+        Console.WriteLine("Contate um Admin para acessar");
+    else
+        Console.WriteLine("Você não tem privilégio o suficiente para acessar");
+}
