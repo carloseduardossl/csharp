24adb8a6c6eecd04292d02ee1f01f49a2720f29a
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-12 10:51:13 -0300

    project 21

--- csharpProjects/csharProject21/obj/Debug/net8.0/csharProject21.AssemblyInfo.cs
+++ csharpProjects/csharProject21/obj/Debug/net8.0/csharProject21.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("csharProject21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd212a683f8bfdeb9488eb86f526ec488d35328")]
-[assembly: System.Reflection.AssemblyProductAttribute("Program")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Program")]
+[assembly: System.Reflection.AssemblyProductAttribute("csharProject21")]
+[assembly: System.Reflection.AssemblyTitleAttribute("csharProject21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
