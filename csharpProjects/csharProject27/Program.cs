99a2440ae41d9f08114d266d2160bba9a4600465
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-13 15:04:53 -0300

    multiplicacao

--- csharpProjects/csharProject27/Program.cs
+++ csharpProjects/csharProject27/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Serialization.Formatters;
+
+int[,] array1 = { { 2, 3 }, { 3, 2 } };
+int[,] array2 = { { 5, 2 }, { 9, 1 } };
+
+int linha1 = array1.GetLength(0);
+int coluna1 = array1.GetLength(1);
+int linha2 = array2.GetLength(0);
+int coluna2 = array2.GetLength(1);
+
+int[,] multiplicacao = new int[linha1, coluna2];
+
+for (int a = 0; a < linha1; a++)
+{
+    for (int b = 0; b < coluna2; b++)
+    {
+        int soma = 0;
+        for (int c = 0; c < coluna1; c++)
+        {
+            soma += array1[a, c] * array2[c, b];
+        }
+        multiplicacao[a, b] = soma;
+    }
+}
+for (int a = 0; a < linha1; a++)
+{
+    for (int b = 0; b < coluna2; b++)
+    {
+        Console.WriteLine($"{multiplicacao[a, b]} ");
+    }
+}
