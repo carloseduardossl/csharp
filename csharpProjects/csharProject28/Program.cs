20d86bda7e92a884af8070d074692de42c61c46f
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-13 17:44:08 -0300

    order and bubble sort

--- csharpProjects/csharProject28/Program.cs
+++ csharpProjects/csharProject28/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numeros = [ 8, 7, 6, 5, 4, 3];
+int n = numeros.Length;
+
+for (int a = 0; a < n - 1; a++)
+{
+    for (int b = 0; b < n - a - 1; b++)
+    {
+        if(numeros[b]>numeros[b + 1])
+        {
+            int temporario = numeros[b];
+            numeros[b] = numeros[b + 1];
+            numeros[b + 1] = temporario;
+        }
+    }
+}
+foreach (int num in numeros)
+{
+    Console.WriteLine(num);
+}
--- csharpProjects/csharProject28/obj/Debug/net8.0/csharProject28.AssemblyInfo.cs
+++ csharpProjects/csharProject28/obj/Debug/net8.0/csharProject28.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharProject28")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef5f39ccc433e83e6b66744078dba15460633dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f9a0ee7492f5d1fba6ce00a0861dc99c00fde6")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharProject28")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharProject28")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
