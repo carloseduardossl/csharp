2a7e3d25a2768692c840876a685d96c76d1bd7d9
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-13 09:10:25 -0300

    for

--- csharpProjects/csharProject22/Program.cs
+++ csharpProjects/csharProject22/Program.cs
@@ -10,3 +10,8 @@ do {
     Console.WriteLine(b);
     b++;
 } while (b < 15);
+
+for (int i = 0; i <= 10; i = i + 2)
+{
+    Console.WriteLine(i);
+}
--- csharpProjects/csharProject22/obj/Debug/net8.0/csharProject22.AssemblyInfo.cs
+++ csharpProjects/csharProject22/obj/Debug/net8.0/csharProject22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharProject22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3136710b5f1ec9559eab544b56cd3c33ed50c615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66c44a3dcbeeb91775fceeadcc9146dad803fae")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharProject22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharProject22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
