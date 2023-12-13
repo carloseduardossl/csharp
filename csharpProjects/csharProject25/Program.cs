1f68618e21869aaae47c350ea1e3e2ff33911d1a
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-13 12:28:58 -0300

    rows and columns

--- csharpProjects/csharProject25/Program.cs
+++ csharpProjects/csharProject25/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] numeros = { { 1, 5, 9 }, { 7, 5, 3 } };
+
+Console.WriteLine(numeros.Length);
+Console.WriteLine(numeros[1, 2]);
+
+numeros[1, 2] = 10;
+
+Console.WriteLine(numeros[1, 2]);
