3136710b5f1ec9559eab544b56cd3c33ed50c615
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-12 21:04:57 -0300

    gg

--- csharpProjects/csharProject21/Program.cs
+++ csharpProjects/csharProject21/Program.cs
@@ -1,20 +1,23 @@
 ﻿bool emailValidacao = true;
-string email = Console.ReadLine();
+string email = "emailtest@gmail.com";
 if (!email.Contains("@"))
     emailValidacao = false;
 else
 {
     string[] emailDividido = email.Split("@");
+        if(emailDividido.Length.Equals(2))
+        {
+            emailValidacao = true;
+        }
     string parte2Email = emailDividido[1];
-    if (!parte2Email.Contains(".com"))
-        emailValidacao = false;
-    else
-        emailValidacao = true;
+    string parte1Email = emailDividido[0]?.Trim();
+    bool test = parte2Email.Contains(".com");
     foreach(char letra in parte2Email){
         if (char.IsNumber(letra)){
             emailValidacao = false;
             break;
         }
     }
+    Console.WriteLine(test);
 }
 Console.WriteLine($"esse email é {(emailValidacao ? "valido" : "invalido")}");
--- csharpProjects/csharProject21/obj/Debug/net8.0/csharProject21.AssemblyInfo.cs
+++ csharpProjects/csharProject21/obj/Debug/net8.0/csharProject21.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharProject21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9a122532afd92f6d4acbf181add95af9898dfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41aef5faf27c531bf593cf3eae80ef3392d008e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharProject21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharProject21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
