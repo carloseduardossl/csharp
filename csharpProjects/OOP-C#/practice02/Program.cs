18a90b781cdb132f84d8e620f6353036a30a7a90
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-16 08:06:07 -0300

    inheritance and encapsulation

---
+++ csharpProjects/OOP-C#/practice02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine($"{myObj.Name} {myObj.Brand}");
+            myObj.Song();
+        }
+    }
+}
