b7ad6c0f9f5d085a3a3520e6fea71d771c70e3a8
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-16 06:45:10 -0300

    gemini

--- csharpProjects/OOP-C#/practice01/Program.cs
+++ csharpProjects/OOP-C#/practice01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace myApp
+{
+    class Program 
+    {
+        static void Main(string[] args) {
+            Person myObj = new Person();
+            myObj.Name = "Gemini";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
