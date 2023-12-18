5e56e68e323a958441385b00e3ddcd1dbb12a582
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-18 07:57:43 -0300

    CheckAge

--- csharpProjects/OOP-C#/practice10/Program.cs
+++ csharpProjects/OOP-C#/practice10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void CheckAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Denied Access! You must be least 18 years old.");
+        }
+        else
+        {
+            Console.WriteLine("Access granted - You are old enough");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        CheckAge(20);
+    }
+}
