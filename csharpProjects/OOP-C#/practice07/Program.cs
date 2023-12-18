48d4d1bc98b56d759e79506d7a0617325f2a60b2
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-18 07:13:47 -0300

    input

--- csharpProjects/OOP-C#/practice07/Program.cs
+++ csharpProjects/OOP-C#/practice07/Program.cs
@@ -6,7 +6,7 @@ namespace App
     {
         static void Main(string[] args)
         {
-            string input = File.ReadAllText("input.txt");
+            string input = File.ReadAllText("input.txt"); // Advent Of Code 2023 Day 01 input
             Console.WriteLine(input);
         }
     }
