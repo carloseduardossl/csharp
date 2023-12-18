8ff7267b34ee58609138093384803b20625e67f1
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-18 06:54:18 -0300

    ReadAllText

--- csharpProjects/OOP-C#/practice07/Program.cs
+++ csharpProjects/OOP-C#/practice07/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            Console.WriteLine(input);
+        }
+    }
+}
