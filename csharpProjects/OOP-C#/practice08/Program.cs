a01e07b8638847484a0d23acaef788afa575a2c7
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-18 07:34:08 -0300

    filililili

--- csharpProjects/OOP-C#/practice08/Program.cs
+++ csharpProjects/OOP-C#/practice08/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+
+string input = File.ReadAllText("../practice07/input.txt");
+Console.WriteLine(input);
+
