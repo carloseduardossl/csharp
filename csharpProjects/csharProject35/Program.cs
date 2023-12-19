9f58d88075c88d80d4a3746ec9a81a38b53a3aa3
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-19 08:46:50 -0300

    circunference and area

---
+++ csharpProjects/csharProject35/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintCircleArea(int radius)
+{
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircunference(int radius)
+{
+    double pi = 3.14159;
+    double cirunference = 2 * pi * radius;
+
+    Console.WriteLine($"Circunference = {cirunference}");
+}
+
+PrintCircleArea(10);
+PrintCircleCircunference(10);
