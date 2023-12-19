086b6adfdc11c9bf1866dd80e0b96428127eb6f6
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-19 11:09:28 -0300

    email

--- csharpProjects/csharProject40/Program.cs
+++ csharpProjects/csharProject40/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"} , {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinna", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody","Dysaty"},
+    {"Shay","Lawrence"},{"daren","Valdes"}
+};
+
+string exertnalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(first: corporate[i, 0], corporate[i, 1]);
+}
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(first: external[i, 0], last: external[i, 1], domain: exertnalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contosco.com")
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
