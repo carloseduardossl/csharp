0411640995642b6efda56ec348a3e34e5aeee9a2
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-19 09:36:01 -0300

    scop reference

--- csharpProjects/csharProject37/Program.cs
+++ csharpProjects/csharProject37/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Scop and reference type
+
+int[] array = { 1, 2, 3, 4, 5 };
+
+PrintArray(array);
+ClearArray(array);
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+void ClearArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
